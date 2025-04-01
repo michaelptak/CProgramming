8a48625d536cfa69a32ed32b3b532d90c5a0646c
Michael Ptak <michael.p11@proton.me>
2025-04-01 10:44:38 -0400

    removed redundant longls

--- CS355/ls/ls2/ls2.c
+++ CS355/ls/ls2/ls2.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <string.h>
 
+void mode_to_string(int mode, char str[]) {
+  strcpy(str, "----------");
+
+  if(S_ISDIR(mode)) str[0] = 'd';
+  if(S_ISCHR(mode)) str[0] = 'c';
+  if(S_ISBLK(mode)) str[0] = 'b';
+  if(S_ISLNK(mode)) str[0] = '1';
+
+  if(mode & S_IRUSR) str[1] = 'r';
+  if(mode & S_IWUSR) str[2] = 'w';
+  if(mode & S_IXUSR) str[3] = 'x';
+  
+  if(mode & S_IRGRP) str[4] = 'r';
+  if(mode & S_IWGRP) str[5] = 'w';
+  if(mode & S_IXGRP) str[6] = 'x';
+
+  if(mode & S_IROTH) str[7] = 'r';
+  if(mode & S_IWOTH) str[8] = 'w';
+  if(mode & S_IXOTH) str[9] = 'x';
+
+}
+
+void show_dir_entry(char *dir_entry_name, struct stat *info){
+  char modestring[11];
+  mode_to_string(info->st_mode, modestring);
+  //printf("Dir entry: %s, size: %ld\n", dir_entry_name, info->st_size);
+  printf("%s ",modestring);
+  printf("%4d ", (int)info->st_nlink);
+  printf("%8d ", (int)info->st_size);
+  printf("%.20s ", ctime(&info->st_mtime));
+  printf("%s\n", dir_entry_name);
+
+}
 
 void do_stat(char *dir_entry_name){
   struct stat info;
@@ -9,7 +44,8 @@ void do_stat(char *dir_entry_name){
     perror(dir_entry_name);
   } 
   else {
-    printf("Dir entry: %s, size: %ld\n", dir_entry_name, info.st_size);
+    show_dir_entry(dir_entry_name, &info);
+    //printf("Dir entry: %s, size: %ld\n", dir_entry_name, info->st_size);
   }
 }
 
